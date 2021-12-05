f14fee551fb71091f448f821ec918b0da0b73e9b
amorist <amor90s.zhang@gmail.com>
2021-12-05 23:09:01 +0800

    Update

--- assimp/assimp.go
+++ assimp/assimp.go
@@ -2,7 +2,7 @@ package assimp
 
 /*
 #cgo CFLAGS: -I${SRCDIR}/assimplib/include -std=c99
-#cgo LDFLAGS: -L${SRCDIR}/assimplib/bin/macos -lassimp -lz -lstdc++
+#cgo LDFLAGS: -L${SRCDIR}/assimplib/bin/macos -lassimp -lstdc++
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,65 +26,44 @@ import (
 	"unsafe"
 )
 
-// Assimp .
-type Assimp struct {
-}
-
-// NewAssimp .
-func NewAssimp() *Assimp {
-	return &Assimp{}
-}
-
 // Import model .
-func (assimp *Assimp) Import(filename string) (scene *C.struct_aiScene, err error) {
-	fmt.Println("filename:", filename)
-	cfilename := C.CString(filename)
+func Import(path string) (scene *C.struct_aiScene, err error) {
+	if path == "" {
+		err = fmt.Errorf("file is empty")
+		return
+	}
+	cfilename := C.CString(path)
 	defer C.free(unsafe.Pointer(cfilename))
 	// C.aiProcess_GlobalScale
 	scene = C.aiImportFile(cfilename, C.aiProcess_Triangulate|C.aiProcess_ValidateDataStructure|C.aiProcess_GlobalScale)
 	if scene == nil {
-		err = fmt.Errorf("Failed to import %s", filename)
+		err = fmt.Errorf("Failed to import %s", path)
 		return
 	}
 	if uintptr(unsafe.Pointer(scene)) == 0 {
-		return nil, fmt.Errorf("Unable to load %s", filename)
+		return nil, fmt.Errorf("Unable to load %s", path)
 	}
 	// make sure we have at least one mesh
 	if scene.mNumMeshes < 1 {
-		return nil, fmt.Errorf("%s no meshes were found", filename)
+		return nil, fmt.Errorf("%s no meshes were found", path)
 	}
-	// for m := 0; m < int(scene.mMetaData.mNumProperties); m++ {
-	// 	mMetaData := C.metadata_at(scene, C.uint(m))
-	// 	fmt.Println(mMetaData)
-	// }
-	// get scene.mMetaData kay value
-	// fmt.Println("scene.mMetaData:", scene.mMetaData)
-	// fmt.Println("scene.mMetaData.mNumProperties:", scene.mMetaData.mNumProperties)
-	// fmt.Println(scene.mMetaData.mKeys)
-
-	// for m := 0; m < int(scene.mNumMeshes); m++ {
-
-	// 	// get the current mesh
-	// 	mesh := C.mesh_at(scene, C.uint(m))
-	// 	fmt.Println(mesh.mVertices.x, mesh.mVertices.y, mesh.mVertices.z)
-	// }
-	// C.aiReleaseImport(scene)
 	return
 }
 
 // Export model.
-func (assimp *Assimp) Export(path string) (err error) {
-	// filename := path[strings.LastIndex(path, "/")+1:]
-	// // get file path with out file name
-	// filepath := path[:strings.LastIndex(path, "/")+1]
-	// filename = filename[:strings.LastIndex(filename, ".")]
-	// fmt.Println("filename:", filename, filepath)
-	scene, err := assimp.Import(path)
-	if err != nil {
+func Export(path, outPath, format string) (err error) {
+	if format == "" {
+		format = "obj"
+	}
+	cfilename := C.CString(path)
+	defer C.free(unsafe.Pointer(cfilename))
+	// C.aiProcess_GlobalScale
+	scene := C.aiImportFile(cfilename, C.aiProcess_ValidateDataStructure|C.aiProcess_GlobalScale)
+	if scene == nil {
+		err = fmt.Errorf("Failed to import %s", path)
 		return
 	}
-	// gltf2
-	aiReturn := C.aiExportScene(scene, C.CString("obj"), C.CString("/Users/amor/Desktop/new/中文/aaa.obj"), C.aiProcess_Triangulate)
+	aiReturn := C.aiExportScene(scene, C.CString(format), C.CString(outPath), C.aiProcess_ValidateDataStructure)
 	if aiReturn != C.aiReturn_SUCCESS {
 		err = fmt.Errorf("Failed to export %s", path)
 		return
--- main.go
+++ main.go
@@ -1,15 +1,67 @@
 package main
 
 import (
-	"flag"
+	"log"
+	"os"
+	"sort"
 
 	"github.com/amorist/assimp-go/assimp"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	a := assimp.NewAssimp()
-	var filename string
-	flag.StringVar(&filename, "f", "", "模型路径")
-	flag.Parse()
-	a.Export(filename)
+	var input, output, format string
+	app := &cli.App{
+		Name:        "assimp-go",
+		Description: "A simple command line tool to convert models to glTF 2.0",
+		Version:     "0.0.1",
+		Commands: []*cli.Command{
+			{
+				Name:    "export",
+				Aliases: []string{"e"},
+				Usage:   "export",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:        "input",
+						Aliases:     []string{"i"},
+						Value:       "",
+						Usage:       "input file",
+						Destination: &input,
+						Required:    true,
+					},
+					&cli.StringFlag{
+						Name:        "output",
+						Aliases:     []string{"o"},
+						Value:       "",
+						Usage:       "output file",
+						Destination: &output,
+						Required:    true,
+					},
+					&cli.StringFlag{
+						Name:        "format",
+						Aliases:     []string{"f"},
+						Value:       "",
+						Usage:       "output format",
+						Destination: &format,
+					},
+				},
+				Action: func(c *cli.Context) error {
+					if len(format) == 0 {
+						format = "obj"
+					}
+					err := assimp.Export(input, output, format)
+					if err != nil {
+						log.Fatal(err)
+					}
+					return nil
+				},
+			},
+		},
+	}
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
